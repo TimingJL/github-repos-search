723d2d92b297ad93778c03a5743060e1ab03e242
taiming <taiming@bridgewell.com>
2020-11-03 05:16:40 +0800

    Updates

---
+++ precache-manifest.23d590075facd18619ec6de3bc271cfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f67bd67b58265c45ead447007d90e0e",
+    "url": "/github-repos-search/index.html"
+  },
+  {
+    "revision": "966988f6c80bb6cadf81",
+    "url": "/github-repos-search/static/css/main.c4a995cf.chunk.css"
+  },
+  {
+    "revision": "43707bb8b1e504c8196f",
+    "url": "/github-repos-search/static/js/2.98b794ff.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/github-repos-search/static/js/2.98b794ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "966988f6c80bb6cadf81",
+    "url": "/github-repos-search/static/js/main.841b99fb.chunk.js"
+  },
+  {
+    "revision": "18b850a0c678b73a4c43",
+    "url": "/github-repos-search/static/js/runtime-main.eaa3f2cd.js"
+  }
+]);
