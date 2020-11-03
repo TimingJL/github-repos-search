6be32dd60acff72a209947fc0b1fef9e86c665d9
taiming <taiming@bridgewell.com>
2020-11-04 04:51:34 +0800

    [feature] add debounceTime for query

--- src/epics/githubReposSearchEpics.tsx
+++ src/epics/githubReposSearchEpics.tsx
@@ -1,11 +1,12 @@
 import { ofType } from 'redux-observable';
 import { of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, debounceTime } from 'rxjs/operators';
 import { FETCH_REPOSITORIES } from 'actions/githubReposSearchActions';
 
 const fetchRepositoriesEpic = action$ => {
 	return action$.pipe(
 		ofType(FETCH_REPOSITORIES),
+		debounceTime(1000),
 		switchMap(({ payload }) => {
 			console.log('epics');
 			return of({
