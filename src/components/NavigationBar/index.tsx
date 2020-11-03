512275ca71d0d54faadf3e18104fc51274cbcca3
taiming <taiming@bridgewell.com>
2020-11-03 19:25:15 +0800

    [feature] add dcard logo

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -5,6 +5,12 @@ import {
 	Brightness4 as Brightness4Icon,
 } from '@material-ui/icons';
 import IconButton from '@material-ui/core/IconButton';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import dcardLogoPath from './dcard_logo.svg';
+
+interface IProps {
+	matches: boolean;
+}
 
 const useStyles = makeStyles(theme => {
 	const {
@@ -14,15 +20,27 @@ const useStyles = makeStyles(theme => {
 	return {
 		root: {
 			height: height,
-			display: 'flex',
-			alignItems: 'center',
-			padding: '0 20px',
 			background: navigationBar.backgroundColor,
 			color: navigationBar.color,
+			boxSizing: 'border-box',
+			display: 'flex',
+			justifyContent: 'center',
 		},
-		title: {
-			flexGrow: 1,
+		container: {
+			display: 'flex',
+			alignItems: 'center',
+			justifyContent: 'space-between',
+			padding: '0 20px',
+			margin: 'auto',
+			boxSizing: 'border-box',
+			width: 1280,
+			maxWidth: 1280,
 		},
+		logo: (props: IProps) => ({
+			display: 'block',
+			height: 28,
+			padding: props.matches ? '0 16px' : 0,
+		}),
 		button: {
 			color: navigationBar.color,
 		},
@@ -35,18 +53,21 @@ interface INavigationBar {
 }
 
 const NavigationBar = ({ isDarkMode, handleChangeTheme }: INavigationBar) => {
-	const classes = useStyles();
+	const matches = useMediaQuery('(min-width: 768px)');
+	const classes = useStyles({ matches });
 	const ThemeIcon = isDarkMode ? Brightness7Icon : Brightness4Icon;
 	return (
 		<div className={classes.root}>
-			<div className={classes.title}>Github Repos Search</div>
-			<IconButton
-				className={classes.button}
-				aria-label="theme change"
-				onClick={handleChangeTheme}
-			>
-				<ThemeIcon />
-			</IconButton>
+			<div className={classes.container}>
+				<img src={dcardLogoPath} className={classes.logo} />
+				<IconButton
+					className={classes.button}
+					aria-label="theme change"
+					onClick={handleChangeTheme}
+				>
+					<ThemeIcon />
+				</IconButton>
+			</div>
 		</div>
 	);
 };
